da0481d7146443bcb1c81470bd0f6d586f318e95
Brahim <brahim.elkarmouchi@gmail.com>
2021-02-05 12:05:44 +0100

    AddArticle

--- app/src/main/java/fr/isen/mahdi/socialnetwork/ArticleActivity.kt
+++ app/src/main/java/fr/isen/mahdi/socialnetwork/ArticleActivity.kt
@@ -4,14 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ArticleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val intent = Intent(this, ArticleActivity::class.java)
-        startActivity(intent)
-    }
+        setContentView(R.layout.activity_article)
 
+    }
 
 
 
